4027900c5e649b4bf91c391f65975b6293e74ead
Andy <andymg@gmail.com>
2022-03-19 19:43:21 +0000

    Created and registered PlatformRepo

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,11 @@
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public interface IPlatformRepo
+{
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformById(int id);
+    void CreatePlatform(Platform platform);
+    bool SaveChanges();
+}
