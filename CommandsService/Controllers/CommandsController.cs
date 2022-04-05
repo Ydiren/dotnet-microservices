fb13af73484e8d083cb6998bada8f8a45b1edc8e
Andy <andymg@gmail.com>
2022-04-05 18:38:55 +0100

    Added first real endpoint to CommandsService and added new CommandsController

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[Route("api/c/platforms/{platformId}/[controller]/")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+    private readonly ICommandRepo _repository;
+    private readonly IMapper _mapper;
+
+    public CommandsController(ICommandRepo repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+    {
+        Console.WriteLine($"--> Hit {nameof(GetCommandsForPlatform)} with platformId '{platformId}'.");
+
+        if (!_repository.PlatformExists(platformId))
+        {
+            return NotFound();
+        }
+
+        var commands = _repository.GetCommandsForPlatform(platformId);
+
+        return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+    }
+}
